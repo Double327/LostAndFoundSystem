d9a56676776e9a502132f68d7f24cc0ef4805290
Double327 <180303665@qq.com>
2020-05-11 10:48:22 +0800

    修复丢失、找到物品信息持久层BUG

--- src/main/java/cn/doublefloat/lostandfound/dao/ILafInformationDao.java
+++ src/main/java/cn/doublefloat/lostandfound/dao/ILafInformationDao.java
@@ -4,6 +4,7 @@ import cn.doublefloat.lostandfound.entity.LafInformation;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,12 +16,11 @@ import java.util.List;
 @Repository("lafInformationDao")
 public interface ILafInformationDao {
 
-
     /**
      * 添加信息
      * @param info 信息
      */
-    @Insert("insert into LAFInformation value (#{id), #{userId}, #{title}, #{type}, #{lostTime}, #{lostPosition}, #{summary}, #{image}, #{createTime}, #{status}")
+    @Insert("insert into LAFInformation values (#{id}, #{userId}, #{title}, #{type}, #{lostTime}, #{lostPosition}, #{summary}, #{image}, #{createTime}, #{status})")
     void addLafInformation(LafInformation info);
 
 
@@ -28,7 +28,7 @@ public interface ILafInformationDao {
      * 删除信息
      * @param id 信息编号
      */
-    @Delete("delete LAFInformation where id = #{id}")
+    @Delete("delete from LAFInformation where id = #{id}")
     void deleteLafInfo(String id);
 
 
@@ -42,9 +42,30 @@ public interface ILafInformationDao {
 
 
     /**
+     * 更改信息状态
+     * @param id 信息编号
+     * @param status 状态信息
+     */
+    @Update("update LAFInformation set status = #{status} where id = #{id}")
+    void updateLafInfoStatus(@Param("id") String id, @Param("status") Integer status);
+
+
+    /**
      * 获取所有信息
      * @return 所有信息
      */
+    @Results(id = "lafInfoResultMap", value= {
+            @Result(property = "id", column = "id"),
+            @Result(property = "userId", column = "user_id"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "type", column = "type"),
+            @Result(property = "lostTime", column = "lost_time"),
+            @Result(property = "lostPosition", column = "lost_position"),
+            @Result(property = "summary", column = "summary"),
+            @Result(property = "image", column = "image"),
+            @Result(property = "createTime", column = "create_time"),
+            @Result(property = "status", column = "status")
+    })
     @Select("select * from LAFInformation")
     List<LafInformation> getAllInformation();
 
@@ -54,6 +75,7 @@ public interface ILafInformationDao {
      * @param id ID
      * @return 信息
      */
+    @ResultMap("lafInfoResultMap")
     @Select("select * from LAFInformation where id = #{id}")
     LafInformation getLafInfoById(String id);
 
@@ -63,7 +85,8 @@ public interface ILafInformationDao {
      * @param title 标题
      * @return 信息
      */
-    @Select("select * from LAFInformation where title like %#{title}%")
-    List<LafInformation> getInfoByName(String title);
+    @ResultMap("lafInfoResultMap")
+    @Select("select * from LAFInformation where title like #{title}")
+    List<LafInformation> getLafInfoByName(String title);
 
 }
--- src/test/java/cn/doublefloat/lostandfound/dao/TestLafInformationDao.java
+++ src/test/java/cn/doublefloat/lostandfound/dao/TestLafInformationDao.java
@@ -1,8 +1,71 @@
 package cn.doublefloat.lostandfound.dao;
 
+import cn.doublefloat.lostandfound.entity.LafInformation;
+import cn.doublefloat.lostandfound.utils.LAFUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author 李广帅
  * @date 2020/5/11 9:27 上午
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestLafInformationDao {
+
+    @Autowired
+    @Qualifier("lafInformationDao")
+    private ILafInformationDao lafInformationDao;
+
+    @Test
+    public void testAddLafInfo() {
+        LafInformation info = new LafInformation(LAFUtils.getId(), LAFUtils.getId(), "丢失一个黑色钱包", 0, new Date(), "江苏省徐州市经济开发区", "黑色、里面有若干现金", "123", new Date(), 0);
+        lafInformationDao.addLafInformation(info);
+    }
+
+    @Test
+    public void testDeleteLafInfo() {
+        lafInformationDao.deleteLafInfo("93345bf429d442ea88e5cef4e6a103bf");
+    }
+
+    @Test
+    public void testUpdateLafInfoTitle() {
+        lafInformationDao.updateLafInfoTitle("57b0eba8b543480b88ea03137cab26f9", "丢失一个黑色白色钱包");
+    }
+
+    @Test
+    public void testUpdateLafInfoStatus() {
+        lafInformationDao.updateLafInfoStatus("57b0eba8b543480b88ea03137cab26f9", 1);
+    }
+
+
+    @Test
+    public void testGetAllLafInfo() {
+        List<LafInformation> lafInformationList = lafInformationDao.getAllInformation();
+        for (LafInformation lafInformation : lafInformationList) {
+            System.out.println(lafInformation);
+        }
+    }
+
+    @Test
+    public void testGetLafInfoById() {
+        LafInformation lafInformation = lafInformationDao.getLafInfoById("57b0eba8b543480b88ea03137cab26f9");
+        System.out.println(lafInformation);
+    }
+
+
+    @Test
+    public void testGetLafInfoByName() {
+        List<LafInformation> lafInformationList = lafInformationDao.getLafInfoByName("%钱包%");
+        for (LafInformation lafInformation : lafInformationList) {
+            System.out.println(lafInformation);
+        }
+    }
 }
